c3a2692ae3ddcc4f2c0b10668ae99499c3ca18b5
sebyakin <a.sebyakin@gmail.com>
2019-12-04 13:27:44 +0300

    First-commit

---
+++ script.js
@@ -0,0 +1,7 @@
+    function skewAll() {
+        document.body.style.transform = "perspective(150px) rotateY(1deg)" ;
+    }
+
+    function skewBack() {
+        document.body.style.transform = "perspective(0) rotateY(0deg)" ;
+    }
